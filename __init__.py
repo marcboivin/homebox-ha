d3f5bb4fde53bba6ee4d430b0ecde5fdacdc24af
Marc Boivin <mboivin@force5.ca>
2025-04-14 20:35:14 -0400

    Claude is having a field day with async_track_event_type... tf type...

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ from homeassistant.helpers.typing import ConfigType
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 from homeassistant.components import persistent_notification
-from homeassistant.helpers.event import async_track_event
+from homeassistant.helpers.event import async_track_state_change_event
 
 from .const import (
     DOMAIN, 
@@ -487,7 +487,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                 ))
     
     # Register area registry update listener
-    coordinator._area_registry_unsub = async_track_event(
+    coordinator._area_registry_unsub = async_track_state_change_event(
         hass, EVENT_AREA_REGISTRY_UPDATED, _handle_area_registry_update
     )
     
